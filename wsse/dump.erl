8182cf9d60b8b9f3c4b94205f92966903b686643
ocao <ocao@www.jin.gr.jp>
2009-02-15 15:02:22 +0900

    erase port8080.erl

--- wsse/dump.erl
+++ wsse/dump.erl
@@ -1,6 +1,6 @@
 #!/usr/bin/env escript
 
 main(_) -> 
-  Latest = getatom:new("kgbu", "ab8b885c35Q", "http://b.hatena.ne.jp/dump"),
+  Latest = getatom:new("username", "password", "http://b.hatena.ne.jp/dump"),
   {ok,FH} = file:open("hatebudump.dat", write),
   io:format(FH, "~s", [Latest]).
